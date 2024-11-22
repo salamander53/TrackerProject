6d1e0535a1b967b1342bd98f8554ea36c5cbd25d
Duy <volydacduy942003@gmail.com>
2024-11-22 17:34:15 +0700

    Add Git LFS configuration and large files

---
+++ Web/electron/main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true
+    }
+  });
+
+  win.loadURL('http://localhost:5173'); // URL của ứng dụng Vite
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
